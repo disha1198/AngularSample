3b63e165a4e63a2247d4bac1de637260f7d9ac28
Disha Shah <disha.shah1198@gmail.com>
2020-12-18 15:53:32 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Name} from './home';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
